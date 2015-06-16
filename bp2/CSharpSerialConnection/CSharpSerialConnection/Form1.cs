678ae7b191fa770ac8fb0efa1cedd4a47217bdf7
Yop Spanjers <yopspanjers@gmail.com>
2015-06-16 12:06:35 +0200

    starterd work on BP2

---
+++ bp2/CSharpSerialConnection/CSharpSerialConnection/Form1.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSharpSerialConnection
+{
+	public partial class Form1 : Form
+	{
+		private SerialPort sPort;
+
+		public Form1 ()
+		{
+			InitializeComponent ();
+			sPort = null;
+		}
+
+		private void RenesSerialDataReceived (object sender, SerialDataReceivedEventArgs e)
+		{
+			textBox1.Invoke (
+				new EventHandler (
+					delegate {
+						MessageBox.Show ("Hello World");
+						string s = sPort.ReadExisting ();
+						richTextBox1.AppendText (s);
+					}
+				)
+			);
+		}
+
+		private void button1_Click (object sender, EventArgs e)
+		{
+			if (sPort == null) {
+				try {
+					sPort = new SerialPort ("/dev/ttyUSB0", 38400, Parity.None, 8, StopBits.One);
+					sPort.DataReceived += new SerialDataReceivedEventHandler (RenesSerialDataReceived);
+					sPort.Open ();
+				} catch (System.IO.IOException) {
+					MessageBox.Show ("Creation failed");
+				}
+			}
+		}
+
+		private void button2_Click (object sender, EventArgs e)
+		{
+			if (sPort != null) {
+				sPort.Write ("Hallo");
+				sPort.Write ("\0");
+			}
+		}
+
+		private void textBox1_KeyPress (object sender, KeyPressEventArgs e)
+		{
+			if (sPort != null) {
+				char[] input = new char[1];
+				input [0] = e.KeyChar;
+				sPort.Write (input, 0, 1);
+			}
+		}
+	}
+}
