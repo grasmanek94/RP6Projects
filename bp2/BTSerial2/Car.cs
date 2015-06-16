cfbb51982c65fec1711730f1eeca196453011c73
Yop Spanjers <yopspanjers@gmail.com>
2015-06-16 12:57:23 +0200

    changes

--- bp2/BTSerial2/Car.cs
+++ bp2/BTSerial2/Car.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using System.IO.Ports;
 
 namespace BTSerial2
 {
 	public class Car
 	{
-		public Car ()
+		public int BatteryLevel { public get; private set; }
+
+		public string Name { public get; private set; }
+
+		public int MaxSpeed { public get; private set; }
+
+
+		public Car (int batteryLevel, string name, int maxSpeed)
 		{
+			BatteryLevel = batteryLevel;
+			Name = name;
+			MaxSpeed = maxSpeed;
+		}
+
+		public void setLeftMotor (int speed, SerialPort port)
+		{
+			
+		}
+
+		public void setRightMotor (int speed, SerialPort port)
+		{
+
+		}
+
+		public void setStop (SerialPort port)
+		{
+			 	
 		}
 	}
 }
