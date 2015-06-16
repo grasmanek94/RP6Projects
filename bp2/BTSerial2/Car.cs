d270f56ab8196229aa3caeec1d08fecbe0a93672
Gamer_Z <githubio@gz0.nl>
2015-06-16 14:54:14 +0200

    update c# serial app

--- bp2/BTSerial2/Car.cs
+++ bp2/BTSerial2/Car.cs
@@ -4,35 +4,174 @@ using System.IO.Ports;
 namespace BTSerial2
 {
 	public class Car
-	{
-		public int BatteryLevel { public get; private set; }
+    {
+        enum Actions
+        {
+            GET_BATTERY_LEVEL = 1,
 
-		public string Name { public get; private set; }
+            SET_MOTOR_L_SPEED,
+            SET_MOTOR_R_SPEED,
 
-		public int MaxSpeed { public get; private set; }
+            MOTOR_STOP,
 
+            GET_MAXIMUM_SPEED,
 
-		public Car (int batteryLevel, string name, int maxSpeed)
+            RESET_MAX_SPEED,
+
+            COMMAND_EXECUTION_SUCCESS,
+            COMMAND_EXECUTION_FAILURE,
+
+            MESSAGECORRUPTION_OCCURED,
+
+            UNKNOWN_COMMAND
+        };
+
+        private SerialPort _SP;
+        private Message writer;
+        private Message reader;
+
+        private System.Timers.Timer _Timer;
+
+        private volatile float _BatteryLevel;
+        private volatile short _MaxSpeed;
+
+        public float BatteryLevel
+        {
+            get { return _BatteryLevel; }
+            private set { _BatteryLevel = value; }
+        }
+
+		public string Name { get; private set; }
+
+        public short MaxSpeed
+        {
+            get { return _MaxSpeed; }
+            private set { _MaxSpeed = value; }
+        }
+
+        public Car (string name, SerialPort port)
 		{
-			BatteryLevel = batteryLevel;
 			Name = name;
-			MaxSpeed = maxSpeed;
-		}
 
-		public void setLeftMotor (int speed, SerialPort port)
+            _SP = port;
+
+            writer = new Message();
+            reader = new Message();
+
+            _Timer = new System.Timers.Timer();
+
+            _Timer.Interval = 100;
+            _Timer.Elapsed += _Timer_Elapsed;
+            _Timer.Enabled = true;
+            _Timer.Start();
+        }
+
+        private void _Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (_SP == null || !_SP.IsOpen)
+            {
+                return;
+            }
+
+            try
+            {
+                if (reader.Read(_SP) > 0)
+                {
+                    switch ((Actions)reader.Action)
+                    {
+                        case Actions.GET_BATTERY_LEVEL:
+                        {
+                            if (reader.DataLen == 4)
+                            {
+                                byte[] data = new byte[4];
+
+                                data[0] = reader.Data[0];
+                                data[1] = reader.Data[1];
+                                data[2] = reader.Data[2];
+                                data[3] = reader.Data[3];
+
+                                BatteryLevel = Convert.ToSingle(data);
+                            }
+                        }
+                        break;
+
+                        case Actions.GET_MAXIMUM_SPEED:
+                        {
+                            if (reader.DataLen == 2)
+                            {
+                                byte[] data = new byte[2];
+
+                                data[0] = reader.Data[0];
+                                data[1] = reader.Data[1];
+
+                                MaxSpeed = Convert.ToInt16(data);
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void setLeftMotor (short speed)
 		{
-			
-		}
+            if (_SP == null || !_SP.IsOpen)
+            {
+                return;
+            }
 
-		public void setRightMotor (int speed, SerialPort port)
+            writer.Action = (byte)Actions.SET_MOTOR_L_SPEED;
+
+            byte[] intBytes = BitConverter.GetBytes(speed);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(intBytes);
+
+            writer.Data[0] = intBytes[0];
+            writer.Data[1] = intBytes[1];
+
+            writer.DataLen = 2;
+
+            writer.Write(_SP);
+        }
+
+		public void setRightMotor (short speed)
 		{
+            if (_SP == null || !_SP.IsOpen)
+            {
+                return;
+            }
 
-		}
+            writer.Action = (byte)Actions.SET_MOTOR_R_SPEED;
 
-		public void setStop (SerialPort port)
+            byte[] intBytes = BitConverter.GetBytes(speed);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(intBytes);
+
+            writer.Data[0] = intBytes[0];
+            writer.Data[1] = intBytes[1];
+
+            writer.DataLen = 2;
+
+            writer.Write(_SP);
+        }
+
+		public void Stop()
 		{
-			 	
-		}
+            if (_SP == null || !_SP.IsOpen)
+            {
+                return;
+            }
+
+            writer.Action = (byte)Actions.MOTOR_STOP;
+
+            writer.DataLen = 0;
+
+            writer.Write(_SP);
+        }
 	}
 }
 
---
+++ bp2/BTSerial2/Message.cs
@@ -0,0 +1,94 @@
+﻿using System.IO.Ports;
+
+namespace BTSerial2
+{
+	public class Message
+	{
+		public byte[] Begin;
+		public byte[] End;
+
+		public byte Action { get; set; }
+
+		public byte DataLen { get; set; }
+
+		public bool PossiblyCorrupt { get; set; }
+
+		public byte[] Data { get; set; }
+
+		public byte[] Corruptioncheck;
+		public byte[] Header;
+
+		public Message ()
+		{
+			Begin = new byte[2];
+			End = new byte[2];
+			Data = new byte[59];
+			Corruptioncheck = new byte[2];
+			Header = new byte[2];
+
+			Begin [0] = (byte)'{';
+			Begin [1] = (byte)'{';
+			End [0] = (byte)'}';
+			End [1] = (byte)'}';
+
+			Action = 0;
+			DataLen = 0;
+			PossiblyCorrupt = false;
+		}
+
+		public Message (byte action)
+			: this ()
+		{
+			Action = action;
+		}
+
+		public override string ToString()
+		{
+			return "Message - " + ((int)Action).ToString () + " - " + ((int)DataLen).ToString () + " - " + PossiblyCorrupt.ToString ();
+		}
+
+        public byte Read(SerialPort port)
+        {
+            if (port.BytesToRead > 5 &&
+                port.ReadByte() == this.Begin[0] &&
+                port.ReadByte() == this.Begin[1])
+            {
+                //Read action & Data lenght
+                this.Action = (byte)port.ReadByte();
+                this.DataLen = (byte)port.ReadByte();
+                if (this.DataLen > 58)
+                {
+                    this.DataLen = 58;
+                }
+                port.Read(this.Data, 0, this.DataLen);
+                this.Data[this.DataLen] = 0;
+
+                //Corruption check
+                port.Read(this.Corruptioncheck, 0, 2);
+
+                this.PossiblyCorrupt = this.Corruptioncheck[0] != this.End[0] || this.Corruptioncheck[1] != this.End[1];
+
+                return (byte)(this.DataLen + 6);
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Write a message to Arduino
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public byte Write(SerialPort port)
+        {
+            this.Header[0] = this.Action;
+            this.Header[1] = this.DataLen;
+
+            port.Write(this.Begin, 0, 2);
+            port.Write(this.Header, 0, 2);
+            port.Write(this.Data, 0, this.DataLen);
+            port.Write(this.End, 0, 2);
+
+            return (byte)(this.DataLen + 6);
+        }
+    }
+}
